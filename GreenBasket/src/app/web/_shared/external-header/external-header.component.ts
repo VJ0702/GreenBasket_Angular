fa374056b73974459ab2fdcf11a83c0368a52c4a
Vijendra Saini <vijendra.saini@dotsquares.com>
2025-03-12 16:56:48 +0530

    -- all categories on home page

--- GreenBasket/src/app/web/_shared/external-header/external-header.component.ts
+++ GreenBasket/src/app/web/_shared/external-header/external-header.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ConfigService } from '../../../services/common-services/config.service';
+import { CategoryService } from '../../../services/category-service/category.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-external-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './external-header.component.html',
   styleUrl: './external-header.component.css'
 })
-export class ExternalHeaderComponent {
+export class ExternalHeaderComponent implements OnDestroy {
+  private getCategorySubscription?: Subscription;
+  categories: any[] = [];
 
+  constructor(private confifService: ConfigService, private categoryService: CategoryService) { }
+
+  ngOnInit(): void {
+    this.fetchCategories();
+  }
+
+  fetchCategories(): void {
+    this.categoryService.getCategories().subscribe(categoryData => {
+      //console.log(categoryData);
+      this.categories = categoryData;  // Assign fetched categories to the categories array
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.getCategorySubscription?.unsubscribe();
+  }
 }
