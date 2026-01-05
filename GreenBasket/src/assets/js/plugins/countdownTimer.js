0379f44b1d028d39caf6e12b34b04f2664817038
Vijendra Saini <vijendra.saini@dotsquares.com>
2026-01-05 18:47:12 +0530

    -- timer changes

--- GreenBasket/src/assets/js/plugins/countdownTimer.js
+++ GreenBasket/src/assets/js/plugins/countdownTimer.js
@@ -13,13 +13,16 @@ function updateTimera() {
     let m = mins - hours * 60;
     let s = secs - mins * 60;
 
-    document.getElementById("dealend").innerHTML =
-        `<div class="dealend-timer">
-            <div class="time-block"><div class="time">${d}</div><span class="day">Days</span></div>
-            <div class="time-block"><div class="time">${h}</div><span class="dots">:</span></div>
-            <div class="time-block"><div class="time">${m}</div><span class="dots">:</span></div>
-            <div class="time-block"><div class="time">${s}</div><span class="dots"></span></div>
-        </div>`;
+    const dealendElement = document.getElementById("dealend");
+    if (dealendElement) {
+        dealendElement.innerHTML =
+            `<div class="dealend-timer">
+                <div class="time-block"><div class="time">${d}</div><span class="day">Days</span></div>
+                <div class="time-block"><div class="time">${h}</div><span class="dots">:</span></div>
+                <div class="time-block"><div class="time">${m}</div><span class="dots">:</span></div>
+                <div class="time-block"><div class="time">${s}</div><span class="dots"></span></div>
+            </div>`;
+    }
 }
 
 // ✅ Use a function reference instead of a string in setInterval
