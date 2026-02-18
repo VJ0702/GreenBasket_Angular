874b69ad328057d7121526fbbd4542263b4e3f8a
Vijendra Saini <vijendra.saini@dotsquares.com>
2026-02-18 18:54:55 +0530

    -- add static data to some pages

--- GreenBasket/src/app/web/_company/contact-us/contact-us.component.ts
+++ GreenBasket/src/app/web/_company/contact-us/contact-us.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { BreadcrumbItem } from '../../../models/common/breadcrumb.model';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { BreadcrumbComponent } from '../../_shared/breadcrumb/breadcrumb.component';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, BreadcrumbComponent],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.css'
 })
 export class ContactUsComponent {
-
+  breadcrumbItems: BreadcrumbItem[] = [
+    { label: 'Home', url: '/' },
+    { label: 'Contact Us', isActive: true }
+  ];
 }
--- GreenBasket/src/app/web/_company/payment-terms/payment-terms.component.ts
+++ GreenBasket/src/app/web/_company/payment-terms/payment-terms.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { BreadcrumbComponent } from '../../_shared/breadcrumb/breadcrumb.component';
+import { BreadcrumbItem } from '../../../models/common/breadcrumb.model';
 
 @Component({
   selector: 'app-payment-terms',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, BreadcrumbComponent],
   templateUrl: './payment-terms.component.html',
   styleUrl: './payment-terms.component.css'
 })
 export class PaymentTermsComponent {
 
+
+  breadcrumbItems: BreadcrumbItem[] = [
+    { label: 'Home', url: '/' },
+    { label: 'Payment Terms', isActive: true }
+  ];
 }
