6ff54e113daf85c00b7dcfc8f910947d8a168b7b
Vijendra Saini <vijendra.saini@dotsquares.com>
2026-02-18 18:28:51 +0530

    -- static data on track order page

--- GreenBasket/src/app/web/_company/track-order/track-order.component.ts
+++ GreenBasket/src/app/web/_company/track-order/track-order.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { BreadcrumbItem } from '../../../models/common/breadcrumb.model';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { BreadcrumbComponent } from '../../_shared/breadcrumb/breadcrumb.component';
 
 @Component({
   selector: 'app-track-order',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, BreadcrumbComponent],
   templateUrl: './track-order.component.html',
   styleUrl: './track-order.component.css'
 })
 export class TrackOrderComponent {
 
+
+  breadcrumbItems: BreadcrumbItem[] = [
+    { label: 'Home', url: '/' },
+    { label: 'Track Order', isActive: true }
+  ];
 }
