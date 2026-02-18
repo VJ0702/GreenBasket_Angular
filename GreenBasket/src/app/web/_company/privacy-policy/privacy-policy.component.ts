e0db9030426c4753f2ff4ca673885d0dfd30fc6c
Vijendra Saini <vijendra.saini@dotsquares.com>
2026-02-18 18:44:16 +0530

    --

--- GreenBasket/src/app/web/_company/privacy-policy/privacy-policy.component.ts
+++ GreenBasket/src/app/web/_company/privacy-policy/privacy-policy.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { BreadcrumbItem } from '../../../models/common/breadcrumb.model';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { BreadcrumbComponent } from '../../_shared/breadcrumb/breadcrumb.component';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, BreadcrumbComponent],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.css'
 })
 export class PrivacyPolicyComponent {
 
+
+  breadcrumbItems: BreadcrumbItem[] = [
+    { label: 'Home', url: '/' },
+    { label: 'Privacy Policy', isActive: true }
+  ];
 }
--- GreenBasket/src/app/web/_company/terms-condition/terms-condition.component.ts
+++ GreenBasket/src/app/web/_company/terms-condition/terms-condition.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { BreadcrumbItem } from '../../../models/common/breadcrumb.model';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { BreadcrumbComponent } from '../../_shared/breadcrumb/breadcrumb.component';
 
 @Component({
   selector: 'app-terms-condition',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, BreadcrumbComponent],
   templateUrl: './terms-condition.component.html',
   styleUrl: './terms-condition.component.css'
 })
 export class TermsConditionComponent {
 
+
+  breadcrumbItems: BreadcrumbItem[] = [
+    { label: 'Home', url: '/' },
+    { label: 'Terms & Conditions', isActive: true }
+  ];
 }
